09b36be2af9f99fdf9387c4343b04d2b1d3802fb
ain <lovepizza132@gmail.com>
2024-07-03 00:01:05 +0900

    feat: msw 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import TextField from './components/TextField';
 import useFetchRestaurants from './hooks/useFetchRestaurants';
+import Restaurant from './types/Restaurant';
 
 export default function App() {
   const restaurants = useFetchRestaurants();
+  console.log(restaurants);
+  // const restaurantsCategory = restaurants.reduce((acc: string[], restaurant: Restaurant) => {
+  //   const { category } = restaurant;
+  //   return acc.includes(category) ? acc : [...acc, category];
+  // }, []);
 
   return (
     <div>
